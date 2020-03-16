389cb582c5b4f71d8a874aa5e209166cf86fa12d
Russell Dorgan <clambakedbenz@gmail.com>
2020-03-16 10:54:39 -0600

    snap 03 practice made a MG to Gram converter
    -added correct answers

--- Snap-03/snap-03-part-2.js
+++ Snap-03/snap-03-part-2.js
@@ -1,6 +1,21 @@
-Input: n=12
-Output: 28
-Dividers sum 1 + 2 + 3 + 4 + 6 + 12 = 28
+// Input: n=12
+// Output: 28
+// Dividers sum 1 + 2 + 3 + 4 + 6 + 12 = 28
 
 //
 
+function sumUniquePositiveFactors(number) {
+	number = Math.abs(number);
+	sum = 0;
+	for (i=1; i<=36; i++) {
+		if (36 % i === 0) {
+			sum = sum + i;
+		}
+	}
+	return sum;
+}
+
+
+
+
+console.log(sumUniquePositiveFactors(36))
--- Snap-03/snap-03.js
+++ Snap-03/snap-03.js
@@ -1,10 +1,29 @@
 //Pseudocode
 
-PROGRAM inputtedEnoughToConvertMGToGram?:
-		IF current_balance - mg_weight > 1000
-			return true
-		ELSE,
-			return false
-		END
-	END
+// PROGRAM inputtedEnoughToConvertMGToGram?:
+// 		IF current_balance - mg_weight > 1000
+// 			return true
+// 		ELSE,
+// 			return false
+// 		END
+// 	END
+
+
+function convertToGrams(weight, units) {
+	if (units === 'lb') {
+		return weight * 453.592;
+	}else if (units === 'kg') {
+		return weight * 1000;
+	}else if (units === 'mg') {
+		return weight / 1000;
+	}else {
+		return weight;
+	}
+}
+
+console.log(convertToGrams(3, "oz"))
+
+//1 = sign: is an assignment
+//2 = sign: allows it to be loose, estimate evaluate
+//3 = sign: no flexibility must be exact
 
