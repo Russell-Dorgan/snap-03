9d2702c1b31e42e67f58eb120d1d1cb5f57a61c9
Russell Dorgan <clambakedbenz@gmail.com>
2020-03-16 09:33:03 -0600

    snap 03 practice made a MG to Gram converter

---
+++ Snap-03/snap-03-part-2.js
@@ -0,0 +1,6 @@
+Input: n=12
+Output: 28
+Dividers sum 1 + 2 + 3 + 4 + 6 + 12 = 28
+
+//
+
---
+++ Snap-03/snap-03.js
@@ -0,0 +1,10 @@
+//Pseudocode
+
+PROGRAM inputtedEnoughToConvertMGToGram?:
+		IF current_balance - mg_weight > 1000
+			return true
+		ELSE,
+			return false
+		END
+	END
+
